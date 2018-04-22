de2747f3dd6719376100a9ba8d699dc44553b21a
root <root@LAPTOP-EMV2T81C.localdomain>
2018-04-22 20:18:19 +0900

    vueの導入

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,44 +1,44 @@
-* eslint no-console: 0 */
-/ Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
-/ <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
-/ to the head of your layout file,
-/ like app/views/layouts/application.html.erb.
-/ All it does is render <div>Hello Vue</div> at the bottom of the page.
+/* eslint no-console: 0 */
+// Run this example by adding <%= javascript_pack_tag 'hello_vue' %> (and
+// <%= stylesheet_pack_tag 'hello_vue' %> if you have styles in your component)
+// to the head of your layout file,
+// like app/views/layouts/application.html.erb.
+// All it does is render <div>Hello Vue</div> at the bottom of the page.
 
-mport Vue from 'vue'
-mport App from '../app.vue'
+import Vue from 'vue'
+import App from './app.vue'
 
-ocument.body.appendchild(document.createElement('hello'))
+document.body.appendChild(document.createElement('hello'))
 
-ew Vue({
+new Vue({
  el: 'hello',
  template: '<App/>',
  components: {App}
-)
+})
 
 
-/ The above code uses Vue without the compiler, which means you cannot
-/ use Vue to target elements in your existing html templates. You would
-/ need to always use single file components.
-/ To be able to target elements in your existing html/erb templates,
-/ comment out the above code and uncomment the below
-/ Add <%= javascript_pack_tag 'hello_vue' %> to your layout
-/ Then add this markup to your html template:
-/
-/ <div id='hello'>
-/   {{message}}
-/   <app></app>
-/ </div>
+// The above code uses Vue without the compiler, which means you cannot
+// use Vue to target elements in your existing html templates. You would
+// need to always use single file components.
+// To be able to target elements in your existing html/erb templates,
+// comment out the above code and uncomment the below
+// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
+// Then add this markup to your html template:
+//
+// <div id='hello'>
+//   {{message}}
+//   <app></app>
+// </div>
 
 
-/ import Vue from 'vue/dist/vue.esm'
-/ import App from '../app.vue'
-/
-/ document.addEventListener('DOMContentLoaded', () => {
-/   const app = new Vue({
-/     el: '#hello',
-/     data: {
-/       message: "Can you say hello?"
+// import Vue from 'vue/dist/vue.esm'
+// import App from '../app.vue'
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   const app = new Vue({
+//     el: '#hello',
+//     data: {
+//       message: "Can you say hello?"
 //     },
 //     components: { App }
 //   })
